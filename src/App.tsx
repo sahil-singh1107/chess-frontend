465b11080a74fec46ba43c4579ebb3344c0af165
sahil-singh1107 <sahil@LAPTOP-TNOQMCGR>
2024-12-30 16:38:27 +0530

    added routing

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Landing from "./pages/Landing"
+import Game from "./pages/Game"
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing/>} />
+          <Route path="/game" element={<Game/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
