ae6a3ee32015bf15b1fe2f5df7c311272fbc5e04
sahil-singh1107 <sahilpython@gmail.com>
2025-01-05 04:37:54 +0530

    added chatbox

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,6 +9,18 @@ import move_self from "../assets/move-self.mp3"
 import illegal_move from "../assets/illegal.mp3"
 import PlayerCard from "@/components/PlayerCard";
 import useClock from "@/hooks/useClock";
+import { LuPlus } from "react-icons/lu";
+import { CiChat1 } from "react-icons/ci";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 
 const Landing = () => {
 
@@ -29,10 +41,10 @@ const Landing = () => {
     const illegal_audio = new Audio(illegal_move);
     const [color, setColor] = useState<string>('white');
     const [chess, setChess] = useState(new Chess())
-    const [started, setStarted] = useState(false);
+    const [started, setStarted] = useState(true);
     const [messages, setMessages] = useState<string[]>([]);
     const [typedMessage, setTypedMessage] = useState<string>("");
-    const [moves, setMoves] = useState<string[]>(["e4", "e4", "e4"])
+    const [moves, setMoves] = useState<string[]>([])
     const { time, isRunning, toggleClock } = useClock()
 
 
@@ -91,9 +103,48 @@ const Landing = () => {
                     <div className="flex justify-between w-full">
                         <PlayerCard username={opponentName} time={600} />
                     </div>
-                    <div className="absolute top-0 left-0">
-                        <p className="text-white">sss</p>
-                    </div>
+                    {
+                        started && (
+                            <div className="absolute top-60 left-20 flex flex-col space-y-5">
+                                <div className="flex flex-col rounded-md p-3 items-center hover:bg-[#303233]">
+                                    <button><LuPlus className="text-white w-10 h-10" /></button>
+                                    <span className="text-[#5c5a59]">New game</span>
+                                </div>
+                                <div className="flex flex-col items-center rounded-md hover:bg-[#303233] p-3">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger><button><CiChat1 className="text-white w-10 h-10" /></button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent className="bg-[#1a1a1d] w-96 h-64">
+                                            <DropdownMenuLabel>{opponentName}</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <div className="flex flex-col overflow-hidden">
+                                                <div className="flex-1 p-4 overflow-y-auto space-y-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+                                                    {
+                                                        moves.map((move, i) => (
+                                                            <div key={i} className=" p-2 rounded-md text-white">{move}</div>
+                                                        ))
+                                                    }
+                                                </div>
+                                                <div className="flex items-center p-4 fixed bottom-0 w-full">
+                                                    <input type="text" value={typedMessage} onChange={(e) => { setTypedMessage(e.target.value) }} className="flex-1 p-2 " placeholder="Type a message..." />
+                                                    <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-400" onClick={() => {
+                                                        socket?.send(JSON.stringify({
+                                                            type: "message",
+                                                            data: { typedMessage }
+                                                        }))
+                                                    }}>Send</button>
+                                                </div>
+                                            </div>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+
+
+                                    <span className="text-[#5c5a59]">Chat</span>
+                                </div>
+
+                            </div>
+                        )
+                    }
                     <div>
                         <GameBoard color={color} socket={socket} chess={chess} setChess={setChess} started={started} />
                     </div>
